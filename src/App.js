0c21784e4faca641e76ba6b99e89b78985331f78
John Pertuz <johnpertuzsism@gmail.com>
2022-02-25 22:38:32 -0500

    Creacion del componente About y su ruta con route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import {Navbar} from './components/Navbar';
-import {Hero} from './components/Hero';
-import {Content} from './components/Content';
+import {Home} from './pages';
+import {About} from './pages/about';
 import {Footer} from './components/Footer';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Hero/>
-      <Content/>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
       <Footer/>
     </div>
   );
