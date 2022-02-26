efd192590dc785270de1a3941d8ab85d71032c65
John Pertuz <johnpertuzsism@gmail.com>
2022-02-25 19:16:04 -0500

    Creacion del componente hero

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import egg from '../images/egg.jpg'
+import egg2 from '../images/egg2.jpg'
+
+const Content = () => {
+  return (
+    <div>
+        <img src={egg} alt="egg" className="h-full rounded mb-20 shadow" />
+    </div>
+  )
+}
+
+export default Content
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-lg"> 
+        <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono" role="navigation"> 
             <Link to='/' className='pl-8'>
                 EGG
             </Link>
