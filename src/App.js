258443c915373cc358090286c54e8c806476afd6
John Pertuz <johnpertuzsism@gmail.com>
2022-02-25 20:13:40 -0500

    Creacion del componente footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {Navbar} from './components/Navbar';
 import {Hero} from './components/Hero';
 import {Content} from './components/Content';
+import {Footer} from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
-      <Content />
+      <Content/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <div className="flex justify-center items-center h-16 bg-black text-white font-mono">
+            <p>Copyrighy © 2022 JohnDev All rights reserved.</p>
+        </div>
+    </div>
+  )
+}
+
